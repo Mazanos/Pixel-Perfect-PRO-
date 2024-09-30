19125a7b4c14d2d4335a5182983700976fbac334
Olga Mazanko <omazanko@itc.intechcard.ru>
2024-09-30 22:08:15 +0300

    lesson2-3

---
+++ lesson2-3/lesson2-3.js
@@ -0,0 +1,8 @@
+const titleProject = "Оценка стоимости" 
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями"
+const screenPrice = 100000
+const percentage = 15
+const fullPrice = 85000
+const responsive = true
+
+console.log(titleProject)
