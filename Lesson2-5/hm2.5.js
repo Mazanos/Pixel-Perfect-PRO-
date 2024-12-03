3a1fe4c790b775d13e1083866bf022e81fc12cd5
Olga Mazanko <omazanko@itc.intechcard.ru>
2024-12-03 16:46:42 +0300

    lesson2-5

---
+++ Lesson2-5/hm2.5.js
@@ -0,0 +1,43 @@
+let screenPrice = 1000
+let persentage = 15/100
+let allServicePrices;
+let newTitle = "";
+let titleProject;
+let screensValue;
+let responsive;
+
+
+
+const asking = function () {
+    titleProject = prompt("Название проекта")
+    screensValue = prompt("Какие типы экранов нужны?")
+    responsive = prompt("Нужен ли респонсив на сайте?")
+}
+
+const checkIsNumber = function (x) {
+    return !isNaN(parseFloat(x)) && isFinite(x)
+
+}
+
+
+const getAllServicePrices = function() {
+    let sum = 0
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            service = prompt("Какой дополнительный тип услуги нужен?", "Услуга 1")
+
+        } else if (i === 1) {
+            serviceSecond = prompt("Какой дополнительный тип услуги нужен?", "Услуга 2")
+        }
+
+        let textFromPrompt = "";
+
+        while (!checkIsNumber(textFromPrompt) || textFromPrompt.trim() === "" || textFromPrompt === null) {
+            textFromPrompt = prompt("Сколько это будет стоить?")
+        }
+        sum = sum + Number(textFromPrompt)
+    
+    }
+    return sum
+}
